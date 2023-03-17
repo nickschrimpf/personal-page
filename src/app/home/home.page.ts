8dbbe1a5f7668283eb1d33fd19438dbde521810a
Nicholas Schrimpf <nickschrimpf@users.noreply.github.com>
2023-03-17 13:42:42 -0600

    Added intro animation w/ dynamic typing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild('greeting',{read:ElementRef,static:true})greeting!: ElementRef;
+  @ViewChild('dynamicTitle',{read:ElementRef,static:true})dynamicTitle!: ElementRef;
 
-  constructor() {}
+
+  constructor(
+    private animationCTRL:AnimationController
+  ) {}
+
+
+  ngAfterViewInit(): void {
+    const greetingAnimation = this.animationCTRL.create()
+    .addElement(this.greeting.nativeElement)
+    .duration(300)
+    .easing('ease-in')
+    .iterations(1)
+    .keyframes([
+       {offset:.0,transform:'scale(2) translateY(-1000%)'},
+       {offset:.70,transform:' scale(1.25) translateY(50%)'},
+       {offset:1,transform:'scale(1.5) translate(0)'}
+    ]);
+      greetingAnimation.play();
+
+    const options ={
+      strings: ['Web Developer','Mobile Developer','UI/UX Designer'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|',
+      backDelay: 1000,
+      startDelay: 1000
+    }
+    new Typed(this.dynamicTitle.nativeElement,options);
+  };
 
 }
+
+
