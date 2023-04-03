577001729f417d62a38c16ebdcee690a98f7a405
Nicholas Schrimpf <nickschrimpf@users.noreply.github.com>
2023-04-03 16:43:52 -0600

    upates to work history

--- src/app/home/work-history/work-history.page.ts
+++ src/app/home/work-history/work-history.page.ts
@@ -1,95 +1,91 @@
-import {  AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ScrollDetail } from '@ionic/angular';
 import { Swiper } from 'swiper';
-import { Subject } from 'rxjs';
-
+import { BehaviorSubject } from 'rxjs';
+import { ScrollService } from '../services/scroll.service';
 
 @Component({
   selector: 'app-work-history',
   templateUrl: './work-history.page.html',
   styleUrls: ['./work-history.page.scss'],
 })
-
 export class WorkHistoryPage implements OnInit, AfterViewInit {
-  @ViewChild('swiper')swiper!:ElementRef;
-  @ViewChild('bgworkHistory')bgworkHistory!:ElementRef;
-  @Input()scrollSubject!:Subject<ScrollDetail>;
+  @ViewChild('swiper') swiper!: ElementRef;
+  @ViewChild('bgworkHistory') bgworkHistory!: ElementRef;
+  @Input() scrollSubject!: BehaviorSubject<ScrollDetail|undefined>;
 
   public swiperController: Swiper | undefined;
   public slideHeight = window.innerHeight;
-  public slideWidth = window.innerWidth*0.8;
-  public mousewheel:any = {
-    sensitivity:0,
-    releaseOnEdges:true,
-    eventsTarget:'.swiper-container',
-    // forceToAxis:true,
-    invert:false,
-  }
-
-
-  public creativeEffect = {
-    limitProgress:2,
-    shadow:true,
-    prev:{
-      translate:['-100%',0,0],
-      opacity:0,
-    },
-    next:{
-      translate:['110%',0,0],
-      opacity:.2,
-    }
+  public slideWidth = window.innerWidth * 0.8;
+  public mousewheel: any = {
+    sensitivity: 0,
+    releaseOnEdges: true,
+    eventsTarget: '.swiper-container',
+    invert: false,
   };
 
+  public creativeEffect = {
+    limitProgress: 2,
+    shadow: true,
+    prev: {
+      translate: ['-100%', 0, 0],
+      opacity: 0,
+    },
+    next: {
+      translate: ['110%', 0, 0],
+      opacity: 0.2,
+    },
+  };
 
-
-
-
-  constructor() { }
+  constructor(public scrollService:ScrollService) {}
 
   ngOnInit() {
+    this.scrollSubject = this.scrollService.scrollSubject;
   }
 
   ngAfterViewInit(): void {
-
-    this.swiperController = new Swiper(this.swiper.nativeElement,{
-      slidesPerView:1,
-      spaceBetween:0,
-      effect:'creative',
-      creativeEffect:this.creativeEffect,
-      speed:1000,
-      parallax:true,
-      on:{
-        init:()=>{
-          if(this.swiperController){
-            this.swiperController.update()
+    this.swiperController = new Swiper(this.swiper.nativeElement, {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      effect: 'creative',
+      creativeEffect: this.creativeEffect,
+      speed: 1000,
+      parallax: true,
+      on: {
+        init: () => {
+          if (this.swiperController) {
+            this.swiperController.update();
           }
-        }
+        },
       },
-
     });
 
-    this.scrollSubject.subscribe((scrollDetail:ScrollDetail)=>{
-      console.log(scrollDetail);
-      if(scrollDetail && scrollDetail.currentY >= this.bgworkHistory.nativeElement.offsetTop){
+    this.scrollSubject.subscribe((scrollDetail: ScrollDetail | undefined) => {
+      if (
+        scrollDetail &&
+        scrollDetail.currentY >= this.bgworkHistory.nativeElement.offsetTop
+      ) {
         this.swiper.nativeElement.swiper.mousewheel.enable();
         this.swiper.nativeElement.swiper.mousewheel.releaseOnEdges = true;
-        console.log(this.swiper.nativeElement.swiper)
-      }else if (
-        scrollDetail && scrollDetail.currentY < this.bgworkHistory.nativeElement.offsetTop ||
-        scrollDetail && scrollDetail.currentY > this.bgworkHistory.nativeElement.offsetTop + this.bgworkHistory.nativeElement.offsetHeight
-        ){
-        console.log('disabled')
-        if(this.swiper.nativeElement.swiper.mousewheel.enabled){
+      } else if (
+        (scrollDetail &&
+          scrollDetail.currentY < this.bgworkHistory.nativeElement.offsetTop) ||
+        (scrollDetail &&
+          scrollDetail.currentY >
+            this.bgworkHistory.nativeElement.offsetTop +
+              this.bgworkHistory.nativeElement.offsetHeight)
+      ) {
+        if (this.swiper.nativeElement.swiper.mousewheel.enabled) {
           this.swiper.nativeElement.swiper.mousewheel.disable();
         }
-
       }
     });
-
   }
-
-
-
-
-
 }
